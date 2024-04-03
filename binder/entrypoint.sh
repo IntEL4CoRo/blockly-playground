e507f6e8bf0000840c5361e97adef8a1f7fdff0b
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-04-03 17:14:53 +0200

    hide left side bar

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,4 +3,6 @@ source ${ROS_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=${PWD}/launch/rvizweb_config/pr2_mujoco.json &
 
+jupyter lab workspaces import  ${PWD}/binder/jupyterlab-workspace.json
+
 exec "$@"
