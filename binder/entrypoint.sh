af6f3870e37a2cf079a245d37b30f154519793db
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-04-19 17:28:19 +0200

    cleanup code for only stretch

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source ${ROS_WS}/devel/setup.bash
 roscore &
-roslaunch --wait rvizweb rvizweb.launch config_file:=${PWD}/launch/rvizweb_config/pr2_mujoco.json &
+roslaunch --wait rvizweb rvizweb.launch config_file:=/home/${NB_USER}/blockly-playground/launch/pr2_mujoco.json &
 
-jupyter lab workspaces import  ${PWD}/binder/jupyterlab-workspace.json
+roslaunch --wait /home/jovyan/blockly-playground/launch/stretch_standalone.launch &
 
 exec "$@"
--- examples/utils.py
+++ examples/utils.py
@@ -1,11 +1,5 @@
-import os
-import psutil
-import subprocess
 import rospy
 from ipywidgets import Button, Layout, GridBox, VBox, Box, FloatSlider, Checkbox, interact
-from IPython.display import display, IFrame, HTML
-from sidecar import Sidecar
-
 from giskardpy.python_interface.python_interface import GiskardWrapper
 from giskardpy.goals.joint_goals import JointPositionList
 from giskardpy.monitors.joint_monitors import JointGoalReached
@@ -13,102 +7,14 @@ from geometry_msgs.msg import Twist, PoseStamped, Point, Quaternion, Vector3Stam
 import roslib; roslib.load_manifest('urdfdom_py')
 from rqt_joint_trajectory_controller import joint_limits_urdf
 
+rospy.init_node('giskard_blockly_playground')
 # Giskard wrapper instance
-gk_wrapper = None
-
-# Directory of the ROS launch files
-LAUNCH_FILE_DIR = os.path.abspath(os.path.join(os.getcwd(), "../launch"))
-# To display visualization tools on the left
-SIDECAR = {
-    'rvizweb': None,
-    'xpra': None
-}
-VIS_TOOLS = {
-    'rvizweb': False,
-    'xpra': False
-}
-# To manage the roslaunch process in the background
-LAUNCH_PROCESS = None
-SELECTED_ROBOT = 'pr2_mujoco'
+gk_wrapper = GiskardWrapper()
 CMD_VEL_TOPIC = '/cmd_vel'
 ROBOT_DESCRIPTION = '/robot_description'
 
-# If it is running on binderhub
-try:
-    JUPYTERHUB_USER = os.environ['JUPYTERHUB_USER']
-except KeyError:
-    JUPYTERHUB_USER = None
-
-# To fix the resize issue of a iframe
-def resizable_iframe(url):
-    return HTML(f"""
-        <div class="iframe-widget" style="width: calc(100% + 10px);">
-            <iframe src="{url}" width="100%", height="100%">
-        </div>
-    """)
-
-# Open rvizweb
-def open_rvizweb():
-    if not VIS_TOOLS['rvizweb']:
-        return False
-    if SIDECAR['rvizweb'] is not None:
-        SIDECAR['rvizweb'].close()
-    try:
-        SIDECAR['rvizweb'] = Sidecar(title='RVIZWEB', anchor='right')
-        with SIDECAR['rvizweb']:
-            display(resizable_iframe(rospy.get_param('/rvizweb/jupyter_proxy_url')))
-    except Exception as e:
-        print('Can not fetch rvizweb url.')
-
-# Open XPRA Desktop
-def open_xpra():
-    if not VIS_TOOLS['xpra']:
-        return False
-    if SIDECAR['xpra'] is not None:
-        return True
-    SIDECAR['xpra'] = Sidecar(title='XRPA', anchor='right')
-    url_prefix = f"/user/{JUPYTERHUB_USER}" if JUPYTERHUB_USER is not None else ''
-    xpra_url = f"{url_prefix}/xprahtml5/index.html"
-    with SIDECAR['xpra']:
-        display(resizable_iframe(xpra_url))
-
-# Execute the roslaunch command
-def _launch_robot(config):
-    global LAUNCH_PROCESS
-    global CMD_VEL_TOPIC
-    global ROBOT_DESCRIPTION
-    if LAUNCH_PROCESS is not None:
-        LAUNCH_PROCESS.terminate()
-        LAUNCH_PROCESS.wait()
-    if 'cmd_vel' in config and config['cmd_vel'] is not None: 
-        CMD_VEL_TOPIC = config['cmd_vel']
-    else:
-        CMD_VEL_TOPIC = '/cmd_vel'
-    if 'robot_description' in config and config['robot_description'] is not None: 
-        ROBOT_DESCRIPTION = config['robot_description']
-    else:
-        ROBOT_DESCRIPTION = '/robot_description'
-        
-    launchfile = os.path.join(LAUNCH_FILE_DIR, f"{config['launchfile']}.launch")
-    command = [
-        'roslaunch',
-        launchfile,
-        'mujoco_suffix:=' + ('' if VIS_TOOLS['xpra'] else '_headless')
-    ]
-    if 'extra_param' in config:
-        command.append(config['extra_param'])
-    open_rvizweb()
-    open_xpra()
-    print("Executing command:\n" + \
-        ' '.join(command) + '\n' + \
-        "in the background, the output will be hidden.\n" + \
-        "To check the output please execute it in a Terminial!")
-    LAUNCH_PROCESS = psutil.Popen(command,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL)
-
 # moving motion
-def move_robot(pos, root_link='map', tip_link='base_link'):
+def add_cartesian_position(pos, root_link='map', tip_link='base_link'):
     pos_stamp = PointStamped()
     pos_stamp.header.frame_id = root_link
     pos_stamp.point = pos
@@ -121,7 +27,7 @@ def move_robot(pos, root_link='map', tip_link='base_link'):
     gk_wrapper.execute()
 
 # rotation motion
-def rotate_robot(ori, root_link='map', tip_link='base_link'):
+def add_cartesian_orientation(ori, root_link='map', tip_link='base_link'):
     ori_stamp = QuaternionStamped()
     ori_stamp.header.frame_id = root_link
     ori_stamp.quaternion = ori
@@ -146,10 +52,10 @@ def add_cartesian_pose(pos, ori, root_link='map', tip_link='base_link'):
         print("Both position and orientation are None!!!")
         return
     if pos is None:
-        rotate_robot(ori, root_link=root_link, tip_link=tip_link)
+        add_cartesian_orientation(ori, root_link=root_link, tip_link=tip_link)
         return
     if ori is None:
-        move_robot(pos, root_link=root_link, tip_link=tip_link)
+        add_cartesian_position(pos, root_link=root_link, tip_link=tip_link)
         return
     pose_stamp = PoseStamped()
     pose_stamp.header.frame_id = root_link
@@ -175,52 +81,6 @@ def get_controlled_joints():
 def get_joint_state():
     return gk_wrapper.world.get_group_info(gk_wrapper.world.get_group_names()[0]).joint_state
 
-
-# Functions for blockly
-def launch_robot(robot, sim_env=None, restart=True):
-    global gk_wrapper
-    robot = robot.upper()
-    robot_dict = {
-        'PR2': {
-            'name': 'PR2',
-            'launchfile': 'pr2_mujoco',
-            'cmd_vel': '/pr2/cmd_vel',
-            'robot_description': '/pr2/robot_description'
-        },
-        'HSR': {
-            'name': 'HSR',
-            'launchfile': 'hsr_mujoco',
-            'cmd_vel': '/hsrb4s/cmd_vel',
-            'robot_description': '/hsrb4s/robot_description'
-        }
-    }
-    env_dict = {
-        'world_containers': '$(find hsr_mujoco)/model/world_containers.xml',
-        'world_only_containers': '$(find hsr_mujoco)/model/world_only_containers.xml',
-        'world_particle_container': '$(find hsr_mujoco)/model/world_particle_container.xml',
-        'Apartment': '$(find mujoco_world)/model/iai_apartment/iai_apartment_with_window4.xml',
-        # 'Waterfront DM': '$(find mujoco_world)/model/waterfront/world.xml',
-        'Kitchen': '$(find mujoco_world)/model/iai_kitchen/iai_kitchen_python.xml',
-    }
-    if LAUNCH_PROCESS is not None and not restart:
-        print("Robot simulator is already running!")
-        return
-
-    if robot in robot_dict:
-        if sim_env is not None and sim_env in env_dict:
-            robot_dict[robot]['extra_param'] = f'mujoco_world:={env_dict[sim_env]}'
-            # if sim_env in ['Apartment', 'Kitchen']:
-            #     VIS_TOOLS['rvizweb'] = False
-            #     VIS_TOOLS['xpra'] = True
-            # else:
-            #     VIS_TOOLS['rvizweb'] = True
-            #     VIS_TOOLS['xpra'] = False
-        _launch_robot(robot_dict[robot])
-        rospy.init_node('giskard_playground')
-        gk_wrapper = GiskardWrapper()
-    else:
-        print(f"Robot {robot} is not available!!!")
-
 # Contorl robot base by cmd_vel message
 def cmd_vel_move(speed, time):
     cmd_vel_pub = rospy.Publisher(CMD_VEL_TOPIC, Twist, queue_size=100)
@@ -239,12 +99,8 @@ def cmd_vel_turn(speed, time):
     rospy.sleep(time)
     cmd_vel_msg.angular.z = 0
     cmd_vel_pub.publish(cmd_vel_msg)
-    
-def debug_func():
-    print('A flexible function for testing')
 
-
-
+# Diplay joint trajectory controller
 def display_joint_trajectory_controller():
     cmd_vel_pub = rospy.Publisher(CMD_VEL_TOPIC, Twist, queue_size=100)
     cmd_vel_msg = Twist()
@@ -292,9 +148,9 @@ def display_joint_trajectory_controller():
             continuous_update=False,
             description=config['name'],
             readout=True,
-            layout=Layout(width='100%'),
+            layout=Layout(width='80%'),
             style=dict(
-                description_width='20rem'
+                description_width='10rem'
             ),
             readout_format='.3f',
         )
